0708daaeedae9d5a99152d385146603b25d4807f
Daniel Andres <dany.eny@yahoo.com>
2025-11-30 17:49:21 +0100

    Füge Formular zur E-Mail-Versendung hinzu und verbessere das Layout

--- basics.js
+++ basics.js
@@ -19,3 +19,20 @@ button.addEventListener("click", () => {
     title.textContent = "Javascript Mini Übung 🕷️";
   }
 });
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  fetch("https://formspree.io/f/xblnlblg", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(() => {
+    window.location.href = "./send_mail.html";
+  }).catch((error) => {
+    console.log(error);
+  });
+}
